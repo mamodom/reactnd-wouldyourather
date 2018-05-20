fb24134377125e9d0136685ce46f7ef1685cdf49
Maximo Dominguez <mamodom@gmail.com>
2018-05-19 22:57:59 -0400

    wip

--- src/helpers/_DATA.js
+++ src/helpers/_DATA.js
@@ -2,8 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL:
-      'https://robohash.org/natusimpeditaliquid.jpg?size=50x50&set=set1',
+    avatarURL: 'https://randomuser.me/api/portraits/women/39.jpg',
     answers: {
       '8xf0y6ziyjabvozdd253nd': 'optionOne',
       '6ni6ok3ym7mf1p33lnez': 'optionOne',
@@ -15,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: 'https://robohash.org/dolorevelnobis.bmp?size=50x50&set=set1',
+    avatarURL: 'https://randomuser.me/api/portraits/men/29.jpg',
     answers: {
       vthrdm985a262al8qx3do: 'optionOne',
       xj352vofupe1dqz9emx13r: 'optionTwo',
@@ -25,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: 'https://robohash.org/eosdictanostrum.jpg?size=50x50&set=set1',
+    avatarURL: 'https://randomuser.me/api/portraits/men/30.jpg',
     answers: {
       xj352vofupe1dqz9emx13r: 'optionOne',
       vthrdm985a262al8qx3do: 'optionTwo',
