8ca27ec05fc27c6d0e0fdaab30bab24a280af74a
Maximo Dominguez <mamodom@gmail.com>
2018-06-20 21:37:30 -0400

    App can be a stateless functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { ConnectedRouter } from 'connected-react-router';
 
 import Auth from './pages/Auth';
 import Home from './pages/Home';
 import QuestionDetails from './pages/QuestionDetails';
+import AddQuestion from './pages/AddQuestion';
 
 import createStore from './store';
 
@@ -14,20 +15,17 @@ const history = createBrowserHistory();
 
 const store = createStore(history);
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div>
-            <Route path="/signin" component={Auth} />
-            <Route exact path="/" component={Home} />
-            <Route path="/questions/:questionId" component={QuestionDetails} />
-          </div>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/signin" component={Auth} />
+        <Route path="/" exact component={Home} />
+        <Route path="/questions/:questionId" component={QuestionDetails} />
+        <Route path="/add" exact component={AddQuestion} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+);
 
 export default App;
