56cd8236d4e4d17abfb4d653517439004e72a50c
Maximo Dominguez <mamodom@gmail.com>
2018-06-22 19:48:28 -0400

    Added styles to login screen

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -4,10 +4,23 @@ import { Redirect } from 'react-router';
 
 import { fetchUsers, login } from '../actions';
 
+import {
+  withStyles,
+  Card,
+  CardContent,
+  Button,
+  Typography,
+  FormControl,
+  MenuItem,
+  Select,
+  InputLabel,
+} from '@material-ui/core';
+
 class Auth extends Component {
   componentDidMount() {
     this.props.fetchUsers();
   }
+
   state = {
     selectedUser: '',
   };
@@ -25,41 +38,81 @@ class Auth extends Component {
 
   render() {
     return (
-      <div>
+      <div className={this.props.classes.root}>
         {this.props.currentUser !== '' && <Redirect to={'/'} />}
-        <h1>Auth</h1>
-        <form onSubmit={this.onSubmit}>
-          <select
-            name="username"
-            id="username"
-            value={this.state.selectedUser}
-            onChange={this.onSelectChange}
-          >
-            <option value="" disabled>
-              Select a user
-            </option>
-            {Object.values(this.props.users).map(user => (
-              <option value={user.id} key={user.id}>
-                {user.name}
-              </option>
-            ))}
-          </select>
-          <button type="submit" disabled={this.state.selectedUser === ''}>
-            Login
-          </button>
-        </form>
+
+        <Card>
+          <CardContent className={this.props.classes.content}>
+            <Typography className={this.props.classes.title} variant="headline">
+              Would you rather...?
+            </Typography>
+
+            <FormControl className={this.props.classes.input}>
+              <InputLabel>User</InputLabel>
+              <Select
+                onChange={this.onSelectChange}
+                value={this.state.selectedUser}
+              >
+                {Object.values(this.props.users).map(user => (
+                  <MenuItem value={user.id} key={user.id}>
+                    {user.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <Button
+              variant="raised"
+              disabled={this.state.selectedUser === ''}
+              onClick={this.onSubmit}
+              color="primary"
+            >
+              Login
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     );
   }
 }
 
-export default connect(
-  ({ users, auth: { currentUser } }) => ({
-    users,
-    currentUser,
-  }),
-  {
-    fetchUsers,
-    login,
-  }
-)(Auth);
+const styles = theme => {
+  return {
+    root: {
+      backgroundColor: theme.palette.primary.main,
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '12em',
+      minWidth: '15em',
+      justifyContent: 'center',
+    },
+    title: {
+      alignSelf: 'center',
+    },
+    input: {
+      marginTop: '1.5em',
+      marginBottom: '1.5em',
+    },
+  };
+};
+
+export default withStyles(styles)(
+  connect(
+    ({ users, auth: { currentUser } }) => ({
+      users,
+      currentUser,
+    }),
+    {
+      fetchUsers,
+      login,
+    }
+  )(Auth)
+);
